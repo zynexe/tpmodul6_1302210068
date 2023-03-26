5a1d645e057a0cbee05c24bf033efe27765fd32f
zynexe <zuhayrraffa@gmail.com>
2023-03-26 18:30:21 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    public int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        this.id = random.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Video ID: {0}", this.id);
+        Console.WriteLine("Title: {0}", this.title);
+        Console.WriteLine("Play Count: {0}", this.playCount);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string videoTitle = "Tutorial Design By Contract – Raffa Zuhayr";
+        SayaTubeVideo video = new SayaTubeVideo(videoTitle);
+
+        video.PrintVideoDetails();
+
+        // tambahkan play count sebanyak 10
+        video.IncreasePlayCount(10);
+
+        Console.WriteLine("\nSetelah ditambahkan 10, play count sekarang adalah {0}", video.playCount);
+    }
+}
